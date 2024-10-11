f61fcf37e0dec416d7360196fc6969c325ca0b57
ALbertEO3 <marichalalberto292@gmail.com>
2024-10-10 23:35:00 -0400

    agregar elemento a la matriz

--- GameOfLife/src/gameoflife/GameOfLife.java
+++ GameOfLife/src/gameoflife/GameOfLife.java
@@ -6,6 +6,7 @@ public class GameOfLife {
 
  
     public static void main(String[] args) {
+      
        
     }
     
--- GameOfLife/src/gameoflife/Matriz.java
+++ GameOfLife/src/gameoflife/Matriz.java
@@ -5,9 +5,20 @@ import java.util.ArrayList;
 public class Matriz{
     private int row;
     private int col;
+    private HashMap<String,Object> matriz;
 
     public Matriz(int row, int col){
         this.row =row;
         this.col = row;
+        this.matriz= new HashMap<>();
+    }
+
+    private void agregar(int fila, int col,Object valor){
+        if (fila >=0 && fila<this.fila && col>=0 && col <this.col){
+            String clave = fila +','+col;
+            matriz.put(clave,valor);
+        } 
+        
+
     }
 }
