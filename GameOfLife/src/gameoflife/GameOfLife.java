0ccce1c97a9074770eaa45d33755a4173aa2bc50
ALbertEO3 <marichalalberto292@gmail.com>
2024-10-11 14:37:22 -0400

    tablero

--- GameOfLife/src/gameoflife/GameOfLife.java
+++ GameOfLife/src/gameoflife/GameOfLife.java
@@ -1,7 +1,20 @@
 package gameoflife;
-
+import java.awt.*;
+import javax.swing.*;
 public class GameOfLife {
 
     public static void main(String[] args) {
+        // faltaria interarlo con la matriz
+        JFrame tablero = new JFrame("Tablero"+1+"x"+1);
+        tablero.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        tablero.setLayout(new GridLayout(10,10));
+        for (int i = 0; i < 10*10; i++) {
+            JPanel panel = new JPanel();
+            panel.setBackground(Color.WHITE);
+            panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            tablero.add(panel);
+        }
+        tablero.setSize(600,600);
+        tablero.setVisible(true);
     }
 }
