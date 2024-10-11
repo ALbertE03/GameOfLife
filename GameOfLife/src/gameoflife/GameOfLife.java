79075bc9cf8e2b0c829d811af6932e4ae4c1ca13
ALbertEO3 <marichalalberto292@gmail.com>
2024-10-11 00:23:55 -0400

    sms

--- GameOfLife/src/gameoflife/GameOfLife.java
+++ GameOfLife/src/gameoflife/GameOfLife.java
@@ -1,13 +1,10 @@
-
 package gameoflife;
-
 
 public class GameOfLife {
 
- 
     public static void main(String[] args) {
-      
-       
+
+        System.out.print("Ya me instalé las cosas pal vscode el netbeans me tenia to loko");
     }
-    
+
 }
